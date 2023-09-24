256327a94c94403f08c146893ec8f99550706846
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-09-25 01:00:22 +0300

    CUB-00
    Modular routing, initialized home and cube controllers, fixed href in navigation for create cube.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const routes = require('./router.js');
 const {port} = require('./constants.js');
 
 const handlebarsConfig = require('./config/handlebarsConfig.js');
@@ -9,8 +9,6 @@ const expressConfig = require('./config/expressConfig.js');
 handlebarsConfig(app);
 expressConfig(app);
 
-app.get('/', (req, res) => {
-    res.render("index", )
-});
+app.use(routes);
 
 app.listen(port, () => console.log(`Server is listening on port: ${port}...`));
