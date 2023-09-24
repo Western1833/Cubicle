7b2be0ca60851bad1207012065010758e145ee3b
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-09-24 21:52:08 +0300

    Update index.js
    Added handlebars and frontpage

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars');
 
 const port = 5050;
 
+app.engine("hbs", handlebars.engine({extname: "hbs"}));
+app.set("view engine", "hbs");
+app.set("views", "src/views")
+
 app.get('/', (req, res) => {
-    res.send('Hello from express server!')
+    res.render("index", )
 })
 
 app.listen(port, () => console.log(`Server is listening on port: ${port}...`))
