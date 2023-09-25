9a65e10e1f69b9bb81140ef385b9277f56fb0346
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-09-25 18:52:05 +0300

    CUB-00
    Added 'uniqid' for generating id's, controllers, cubeService with 'cubes' as DB and 'createCube', 'getAllCubes', fixed 'action' and 'method' in form for create.hbs

---
+++ src/services/cubeService.js
@@ -0,0 +1,31 @@
+const uniqid = require('uniqid');
+const cubes = [
+    {
+      id: 'da47ikbuklmz1ycro',
+      name: 'Normal Rubik cube',
+      description: 'Normal',
+      imageUrl: 'https://m.media-amazon.com/images/I/51NQZl6VNzL.jpg',
+      difficultyLevel: 1
+    },
+    {
+      id: 'da47ikbuklmz255r3',
+      name: 'Rubik cube',
+      description: 'Hard to solve cube.',
+      imageUrl: 'https://images.interestingengineering.com/images/JULY/Erno_Rubik_Magic_Cube.jpg',
+      difficultyLevel: 4
+    }
+  ];
+
+exports.createCube = (cubeData) => {
+    const newCube = {
+        id: uniqid(),
+        ...cubeData
+    };
+
+    cubes.push(newCube);
+    return newCube;
+}
+
+exports.getAllCubes = () => {
+    return [...cubes];
+}
